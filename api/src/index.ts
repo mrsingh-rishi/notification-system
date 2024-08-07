26e0d435af9f83f260658dc8be91cc1283482939
Rishi Singh <rs3949427@gmail.com>
2024-08-07 23:33:05 +0530

    created api for the notification service

---
+++ api/src/index.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { Kafka } from "kafkajs";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    const { service, to, message } = req.body;
+    await producer.connect();
+
+    await producer.send({
+      topic: service,
+      messages: [
+        {
+          value: JSON.stringify({ to, message }),
+        },
+      ],
+    });
+
+    res.status(201).send("Notification sent successfully");
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Failed to send notification" });
+  }
+});
+
+app.listen(port);

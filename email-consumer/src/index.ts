e5f7d5833b216304d648eec67be7f0f5ad2c6f24
Rishi Singh <rs3949427@gmail.com>
2024-08-07 23:41:01 +0530

    initialise the email consumer

---
+++ email-consumer/src/index.ts
@@ -0,0 +1,25 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "test-group" });
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "email", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      });
+    },
+  });
+};
+
+run().catch(console.error);
